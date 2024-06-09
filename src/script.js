8101d75c9b9a18d64c40e607eca57402838414d2
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-06-09 01:00:03 -0500

    add material

--- src/script.js
+++ src/script.js
@@ -13,11 +13,23 @@ const gui = new GUI();
  */
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(concretePlasticContactMaterial);
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: plasticMaterial,
 });
 
 world.addBody(sphereBody);
@@ -25,6 +37,7 @@ const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
 floorBody.mass = 0;
 floorBody.addShape(floorShape);
+floorBody.material = concreteMaterial;
 world.addBody(floorBody);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 
